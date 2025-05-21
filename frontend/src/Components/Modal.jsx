aa6f7a81232562487bef0716a6ddece7549e3f75
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-21 10:27:18 +0530

    Setting Ui

---
+++ frontend/src/Components/Modal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Modal = ({ children, isOpen, onClose, hideHeader }) => {
+  return <div>Modal</div>;
+};
+
+export default Modal;
--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,8 +1,141 @@
-import React from "react";
-import HeroImg from "../assets/hero-img.jpg";
-
+/* eslint-disable no-unused-vars */
+import HeroImg from "../assets/hero-imd1.jpg";
+import { APP_FEATURES } from "../Util/data.js";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LuSparkles } from "react-icons/lu";
+import Modal from "../Components/Modal.jsx";
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const navigate = useNavigate();
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setcurrentPage] = useState("login");
+
+  const handleCTA = () => {};
+
+  return (
+    <>
+      <div className="w-full min-h-full bg-[#FFFCEF] relative overflow-hidden mb-56">
+        {/* Full-page amber blur background */}
+        <div className="absolute top-0 left-0 w-[700px] h-[700px] bg-amber-200/20 blur-[65px] z-0" />
+
+        {/* Main content */}
+        <div className="container mx-auto px-4 pt-6 pb-[200px] relative z-10">
+          {/* Header */}
+          <header className="flex justify-between items-center mb-[120px]">
+            <div className="text-4xl text-black font-bold flex gap-4">
+              <img src={"/favicon.svg"} alt="Logo" height={100} width={100} />
+              HireWire
+            </div>
+            <button
+              className="bg-black text-md font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-400 transition-colors cursor-pointer"
+              onClick={() => setOpenAuthModal(true)}
+            >
+              Login / SignUp
+            </button>
+          </header>
+
+          {/* Hero Section */}
+          <div className="flex flex-col md:flex-row items-center justify-between">
+            {/* Left Side - Badge and Heading */}
+            <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+              <div className="flex items-center justify-start mb-2">
+                <div className="flex items-center gap-2 text-[14px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border-amber-300">
+                  <LuSparkles className="text-amber-600" />
+                  AI Powered
+                </div>
+              </div>
+              <h1 className="text-5xl text-black font-semibold leading-tight">
+                Ace Interviews with <br />
+                <span className="text-transparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%,#FCD760_100%)] bg-[length:200%_200%] animate-text-shine font-semibold">
+                  AI Powered
+                </span>{" "}
+                Learning
+              </h1>
+            </div>
+
+            {/* Right Side - Paragraph and Button */}
+            <div className="w-full md:w-1/2">
+              <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6 font-semibold">
+                Get role-specific questions, expand answers when you need them,
+                dive deeper into concepts and organise your way, drive from
+                preparation to mastery - Your ultimate interview toolkit is here
+              </p>
+
+              <button
+                className="bg-black text-md font-semibold text-white px-7 py-2.5 rounded-full hover:bg-yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-400 transition-colors cursor-pointer"
+                onClick={handleCTA}
+              >
+                Get Started!
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full min-h-full relative z-10">
+        <div>
+          <section className="flex justify-center items-center -mt-59">
+            <img src={HeroImg} alt="Hero img" className="w-[75vw] rounded-xl" />
+          </section>
+        </div>
+      </div>
+
+      <div className="w-full min-h-full bg-[#FFFCEF] mt-10">
+        <div className="container mx-auto px-4 pt-10 pb-20">
+          <section className="mt-5">
+            <h2 className="text-4xl font-medium text-center mb-12">
+              Features That Make You Shine
+            </h2>
+            <div className="flex flex-col items-center gap-8">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+                {APP_FEATURES.slice(0, 3).map((feature) => (
+                  <div
+                    key={feature.id}
+                    className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-200 transition border border-amber-300"
+                  >
+                    <h3 className="text-base font-semibold mb-3">
+                      {feature.title}
+                    </h3>
+                    <p className="text-gray-600">{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {APP_FEATURES.slice(3).map((feature) => (
+                  <div
+                    key={feature.id}
+                    className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-200 transition border border-amber-300"
+                  >
+                    <h3 className="text-base font-semibold mb-3">
+                      {feature.title}
+                    </h3>
+                    <p className="text-gray-600">{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <Modal
+        isOpen={openAuthModal}
+        onClose={() => {
+          setOpenAuthModal(false);
+          setcurrentPage("login");
+        }}
+        hideHeader
+      >
+        <div>
+          {currentPage == "login" && <Login setcurrentPage={setcurrentPage} />}
+          {currentPage == "signup" && (
+            <Signup setcurrentPage={setcurrentPage} />
+          )}
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default LandingPage;
